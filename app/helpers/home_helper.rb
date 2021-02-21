0dd480db090123d9bdbcfbdcaab852ddf777301b
Lukasz Fuszara <lukasz@fuszara.pl>
2021-02-21 19:01:43 +0100

    Portfolio v0.0.2

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,17 +4,10 @@ module HomeHelper
 
   def parse_images(img, name)
     hash = img.as_json
+    name_sym = name.to_sym
 
-    case name
-    when 'logo'
-      img_logo = img.logo
-      hash[:logo] = url_for img_logo
-    when 'avatar'
-      img_logo = img.avatar
-      hash[:avatar] = url_for img_logo
-    else
-      raise ArgumentError
-    end
+    img_logo = img.send(name_sym)
+    hash[name_sym] = url_for img_logo
 
     hash
   end
